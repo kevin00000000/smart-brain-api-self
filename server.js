958351f1e972fe0c6c5470e10335d10c65d32129
Kevin00000000 <jiannan_yang@163.com>
2018-10-17 15:40:55 +0800

    connect heroku postgres

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const clarifaiHandler = require('./controllers/clarifaiHandler')
 const db = knex({
     client: 'pg',
     connection: {
-        host: '127.0.0.1',
-        user:'postgres',
-        password: '123456',
-        database: 'smart-brain-self'
+        connectionString: process.env.DATABASE_URL,
+        ssl: true
     }
 });
 
@@ -33,3 +31,24 @@ app.post('/clarifai', clarifaiHandler())
 app.listen(process.env.PORT, () => {
     console.log(`app is running on port ${process.env.PORT}`)
 })
+
+/*
+CREATE TABLE public.logins (
+	id serial NOT NULL,
+	email text NOT NULL,
+	"password" text NOT NULL,
+	CONSTRAINT logins_email_key UNIQUE (email),
+	CONSTRAINT logins_pkey PRIMARY KEY (id)
+);
+
+CREATE TABLE public.users (
+	id serial NOT NULL,
+	"name" varchar(100) NOT NULL,
+	email text NOT NULL,
+	entries int2 NULL DEFAULT 0,
+	joined date NOT NULL,
+	CONSTRAINT users_email_key UNIQUE (email),
+	CONSTRAINT users_pkey PRIMARY KEY (id),
+	CONSTRAINT users_logins_fk FOREIGN KEY (email) REFERENCES logins(email)
+);
+*/
